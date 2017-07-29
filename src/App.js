73943183f05d9c68d9dd7a4ba7707f83246465c9
Matthew-Burfield <m1.burfield@gmail.com>
2017-07-29 16:31:53 +1000

    removed infinite scroller

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import InfiniteScroll from 'react-infinite-scroller';
 import './App.css';
 
+const colors = [
+  'blue',
+  'red',
+  'white',
+  'black',
+  'green',
+  'yellow',
+  'purple',
+  'brown',
+  'darkblue',
+  'darkgreen',
+  'darkred',
+  'gray',
+  'blue',
+  'red',
+  'white',
+  'black',
+  'green',
+  'yellow',
+  'purple',
+  'brown',
+  'darkblue',
+  'darkgreen',
+  'darkred',
+  'gray'
+];
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+    }
+  }
+  componentDidMount = () => {
+    this.setState(prevState => ({
+      items: prevState.items.length === 0 ? [] : prevState.push(...colors),
+    }));
+  }
+  loadMore = () => {
+    this.setState(prevState => ({
+      items: prevState.push(...colors),
+    }));
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadMore}
+          useWindow={false}
+        >
+          {this.state.items.map(item => (
+            <div style={{backgroundColor: {item}, height: 200}}/>
+          ))}
+        </InfiniteScroll>
       </div>
     );
   }

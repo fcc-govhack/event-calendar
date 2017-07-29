473d139fbb6d7ce5b7b5cc3002bfbe17c27ba56c
Greg Duncan <Greg Duncan>
2017-07-29 18:06:55 +1000

    stuff

--- src/EventCard.js
+++ src/EventCard.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 const EventCard = ({ event }) => (
-  <div className='event-card'>
-    <div className='title'>{event.attributes.Program_Title}</div>
-    <div className='description'>{event.attributes.Program_Description}</div>
-    <div className='day'>{event.attributes.Day}</div>
-    <div className='time'>{event.attributes.Time}</div>
-    <div className='cost'>{event.attributes.Cost}</div>
-  </div>
+  // <div className='event-card'>
+  //   <div className='title'>{event.attributes.Program_Title}</div>
+  //   <div className='description'>{event.attributes.Program_Description}</div>
+  //   <div className='day'>{event.attributes.Day}</div>
+  //   <div className='time'>{event.attributes.Time}</div>
+  //   <div className='cost'>{event.attributes.Cost}</div>
+  // </div>
+  <div className='row event-instance'>
+        <div className='col-lg-6'>
+            <img className='img-responsive' src='' />
+            <div className='event-details'>
+                <div className='event-title'>
+                    <p className='event-text'>{event.attributes.Program_Title}</p>
+                </div>
+                <div className='event-date'>
+                    <p className='event-description-container'>{event.attributes.Program_Description}</p>
+                </div>
+            </div>
+        </div>
+    </div>   
 );
 
 export default EventCard;

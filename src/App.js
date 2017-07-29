4d469aa3aa9e1042a6af4f5945703cb639178b5e
Matthew-Burfield <m1.burfield@gmail.com>
2017-07-29 20:20:52 +1000

    added images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { Component } from 'react'
 
 import EventCard from './EventCard'
+import logo from './images/logo.jpg'
 
 const dataUrl = 'https://services5.arcgis.com/ZUCWDRj8F77Xo351/arcgis/rest/services/Logan_City_Council_Community_Centre_Programs_2017/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&sqlFormat=none&f=pjson&token='
 
@@ -24,8 +25,15 @@ class App extends Component {
     });
   }
   render() {
+    console.log(logo)
     return (
-      <div className='container'>
+      <div>
+        <div className="header">
+          <div className="header-logo">
+            <img className="logo-img" src={logo} alt='App logo' />
+            <p className="logo">EVENTURER</p>
+          </div>
+        </div>
         {this.state.data.map(event => <EventCard key={event.attributes.FID} event={event} />)}
       </div>
     );
--- src/EventCard.js
+++ src/EventCard.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import farmersmarket from './images/farmersmarket.jpg'; 
+import farmersmarketRollover from './images/farmersmarket-rollover.jpg'; 
+import button from './images/btn.png';
+
 const EventCard = ({ event }) => (
   // <div className='event-card'>
   //   <div className='title'>{event.attributes.Program_Title}</div>
@@ -8,19 +12,34 @@ const EventCard = ({ event }) => (
   //   <div className='time'>{event.attributes.Time}</div>
   //   <div className='cost'>{event.attributes.Cost}</div>
   // </div>
-  <div className='row event-instance'>
-        <div className='col-lg-6'>
-            <img className='img-responsive' src='https://images.unsplash.com/photo-1498491480129-04f6d95c90be' alt='eventphoto'/>
-            <div className='event-details'>
-                <div className='event-title'>
-                    <p className='event-text'>{event.attributes.Program_Title}</p>
-                </div>
-                <div className='event-date'>
-                    <p className='event-description-container'>{event.attributes.Program_Description}</p>
-                </div>
+  <div className="container">
+    <div className="event-container">
+      <div className="row event-instance">
+        <div className="col-lg-6">
+          <div className="date-box">AUG 6</div>
+          <div className="cf clearfix">
+            <img className="top" src={farmersmarket} alt='' />
+            <img className="bottom" src={farmersmarketRollover} alt='' />
+          </div>
+          <div className="event-details">
+            <div className="event-title">
+              <p className="event-text">{event.attributes.Program_Title}</p>
             </div>
+            <div className="event-description-container">
+              <p className="event-description">{event.attributes.Program_Description}</p>
+              <p className ="see-more">...see more</p>
+            </div>
+          </div>
+          <div className="event-specs">
+            <p className="event-specs-detail">Date & Time: <br /><b>August 6</b><br /><b>6am - 12pm</b><br /><br /> Location: <br/><b>Beenleigh Showgrounds</b><br /><br /> Cost:<br /><b>Free!</b><br /><br /><br />
+              <img src={button} alt='' />
+              <img src={button} alt='' />
+            </p>
+          </div>
         </div>
-    </div>   
+      </div>
+    </div>
+  </div>
 );
 
 export default EventCard;

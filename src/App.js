dc6996324361087cf71e656027c74f5ddb50307f
Matthew-Burfield <m1.burfield@gmail.com>
2017-07-29 18:52:34 +1000

    fixed app.css error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import React, { Component } from 'react'
 
 import EventCard from './EventCard'
-import './App.css'
 
 const dataUrl = 'https://services5.arcgis.com/ZUCWDRj8F77Xo351/arcgis/rest/services/Logan_City_Council_Community_Centre_Programs_2017/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&sqlFormat=none&f=pjson&token='
 
